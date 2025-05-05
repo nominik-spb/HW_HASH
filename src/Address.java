71c66fb82387ed7fddb55b7a443dbfc4f6f03dd3
nominik-spb <61251151+nominik-spb@users.noreply.github.com>
2025-05-05 23:16:53 +0300

    1

--- src/Address.java
+++ src/Address.java
@@ -1,11 +1,24 @@
-public class Adress {
+import java.util.Objects;
+
+public class Address {
 
     public String country;
     public String city;
 
-    public Adress (String country, String city) {
+    public Address(String country, String city) {
         this.country = country;
         this.city = city;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(country, address.country) && Objects.equals(city, address.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country, city);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.HashMap;
+import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Map<Address, Integer> costPerAddress = new HashMap<>();
+        int total = 0;
+        Set<String> countries = new HashSet<>();
+
+        Address delivery1 = new Address("Россия", "Санкт-Петербург");
+        costPerAddress.put(delivery1, 100);
+
+        Address delivery2 = new Address("Россия", "Москва");
+        costPerAddress.put(delivery2, 150);
+
+        Address delivery3 = new Address("Индия", "Дели");
+        costPerAddress.put(delivery3, 830);
+
+        Address delivery4 = new Address("Китай", "Пекин");
+        costPerAddress.put(delivery4, 910);
+
+        Address delivery5 = new Address("Германия", "Франкфурт");
+        costPerAddress.put(delivery5, 520);
+
+        while (true) {
+            System.out.print("Заполнение нового заказа.\n" +
+                    "Введите страну: ");
+            String country = sc.nextLine();
+            if ("end".equals(country)) {
+                break;
+            }
+            System.out.print("Введите город: ");
+            String city = sc.nextLine();
+            if ("end".equals(city)) {
+                break;
+            }
+            System.out.print("Введите вес (кг): ");
+            int weight = Integer.parseInt(sc.nextLine());
+
+            Address address = new Address(country, city);
+            if (costPerAddress.containsKey(address)) {
+                int price = costPerAddress.get(address) * weight;
+                total += price;
+                countries.add(country);
+                System.out.printf("Стоимость доставки составит: %d руб.\n", price);
+                System.out.printf("Общая стоимость всех доставок: %d руб.\n", total);
+                System.out.printf("Количество посещенных стран: %d\n", countries.size());
+            } else {
+                System.out.println("Доставки по такому адресу нет.");
+            }
         }
     }
 }
