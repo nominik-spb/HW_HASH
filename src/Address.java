d2725dd1da76a25fdb651d858c74d44ab7067614
nominik-spb <61251151+nominik-spb@users.noreply.github.com>
2025-05-05 23:16:02 +0300

    Initial commit

---
+++ src/Address.java
@@ -0,0 +1,11 @@
+public class Adress {
+
+    public String country;
+    public String city;
+
+    public Adress (String country, String city) {
+        this.country = country;
+        this.city = city;
+    }
+
+}
